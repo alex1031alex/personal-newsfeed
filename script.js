81179c6461aa23d282728e910b7e5ea774fc838d
Artur Stambultsian <bumbledos@gmail.com>
2021-09-21 19:24:24 +0300

    more components

--- script.js
+++ script.js
@@ -14,17 +14,17 @@ const categoryNames = {
   fashion: 'Мода',
 }
 
-const Navigation = ({ pages = [], currentPage, className = '', onNavClick = () => {}, placement, ...restProps }) => {
+const Navigation = ({ categories = [], currentCategory, className = '', onNavClick = () => {}, placement, ...restProps }) => {
   return (
     <nav {...restProps} className={`grid navigation navigation--${placement} ${className}`}>
       <a className="navigation__logo" onClick={onNavClick} data-href="index" href="/index.html">
         <img className="navigation__logo-image" src="./images/logo.svg" alt="Логотип"/>
       </a>
       <ul className="navigation__list">
-        {pages.map((page) => {
+        {categories.map((page) => {
           return (
             <li className="navigation__item" key={page}>
-              <a className={`navigation__link ${page === currentPage ? 'navigation__link--active' : ''}`} onClick={onNavClick} data-href={page} href="#">{categoryNames[page]}</a>
+              <a className={`navigation__link ${page === currentCategory ? 'navigation__link--active' : ''}`} onClick={onNavClick} data-href={page} href="#">{categoryNames[page]}</a>
             </li>
           )
         })}
@@ -33,21 +33,84 @@ const Navigation = ({ pages = [], currentPage, className = '', onNavClick = () =
   )
 }
 
+const MainArticle = ({ title, image, description, category, source }) => {
+  return (
+    <article className="main-article">
+      <div className="main-article__image-container">
+        <img className="article-img main-article__img" src={image} alt="Фото новости"/>
+      </div>
+      <div className="main-article__content">
+        <span className="article-category">{category}</span>
+        <h2 className="main-article__title">{title}</h2>
+        <p className="main-article__text">{description}</p>
+        <span
+          className="article-source main-article__caption">{source}</span>
+      </div>
+    </article>
+  )
+}
+
+const SmallArticle = ({ title, date, source }) => {
+  return (
+    <article className="small-article">
+      <h2 className="small-article__title">{title}</h2>
+      <span
+        className="article-date">{source}</span>
+      <span className="article-source">{new Date(date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' })}</span>
+    </article>
+  )
+}
+
+const Articles = ({ data }) => {
+  return (
+    <section className="articles">
+      <div className="container grid">
+        <section className="articles__big-column">
+          {data.items.slice(0, 3).map(({ title, image, description, category_id, source_id }) => {
+            return (
+              <MainArticle
+                key={title}
+                title={title}
+                image={image}
+                description={description}
+                category={data.categories.find(({id}) => category_id === id).name}
+                source={data.sources.find(({id}) => source_id === id).name}
+              />
+            )
+          })}
+        </section>
+        <section className="articles__small-column">
+          {data.items.slice(3, 12).map(({ title, date, source_id  }) => {
+            return (
+              <SmallArticle
+                key={title}
+                title={title}
+                date={date}
+                source={data.sources.find(({id}) => source_id === id).name}
+              />
+            )
+          })}
+        </section>
+      </div>
+    </section>
+  )
+}
+
 const App = () => {
-  const [page, setPage] = React.useState('index');
+  const [category, setCategory] = React.useState('index');
   const [data, setData] = React.useState({ items: [], categories: [], sources: [] });
 
   React.useEffect(() => {
-    fetch(`http://frontend.karpovcourses.net/api/v2/ru/news/${categoryIds[page]}`)
+    fetch(`http://frontend.karpovcourses.net/api/v2/ru/news/${categoryIds[category]}`)
       .then(response => response.json())
       .then(({ items, categories, sources }) => {
         setData({ items, categories, sources })
       })
-  }, [page]);
+  }, [category]);
 
   const onNavClick = (e) => {
     e.preventDefault();
-    setPage(e.currentTarget.dataset.href);
+    setCategory(e.currentTarget.dataset.href);
   }
 
   return (
@@ -56,48 +119,15 @@ const App = () => {
         <div className="container">
           <Navigation
             placement="header"
-            pages={['index', 'fashion', 'technologies', 'sport', 'karpov']}
+            categories={['index', 'fashion', 'technologies', 'sport', 'karpov']}
             onNavClick={onNavClick}
-            currentPage={page}
+            currentCategory={category}
           />
         </div>
       </header>
 
       <main>
-        <section className="articles">
-          <div className="container grid">
-            <section className="articles__big-column">
-              {data.items.slice(0, 3).map((item) => {
-                return (
-                  <article className="main-article" key={item.title}>
-                    <div className="main-article__image-container">
-                      <img className="article-img main-article__img" src={item.image} alt="Фото новости"/>
-                    </div>
-                    <div className="main-article__content">
-                      <span className="article-category">{data.categories.find(({id}) => item.category_id === id).name}</span>
-                      <h2 className="main-article__title">{item.title}</h2>
-                      <p className="main-article__text">{item.description}</p>
-                      <span
-                        className="article-source main-article__caption">{data.sources.find(({id}) => item.source_id === id).name}</span>
-                    </div>
-                  </article>
-                )
-              })}
-            </section>
-            <section className="articles__small-column">
-              {data.items.slice(3, 12).map((item) => {
-                return (
-                  <article className="small-article" key={item.title}>
-                    <h2 className="small-article__title">{item.title}</h2>
-                    <span
-                      className="article-date">{data.sources.find(({id}) => item.source_id === id).name}</span>
-                    <span className="article-source">{new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' })}</span>
-                  </article>
-                )
-              })}
-            </section>
-          </div>
-        </section>
+        <Articles data={data}/>
       </main>
 
       <footer className="footer">
@@ -105,9 +135,9 @@ const App = () => {
           <Navigation
             placement="footer"
             className="footer__navigation"
-            pages={['index', 'fashion', 'technologies', 'sport', 'karpov']}
+            categories={['index', 'fashion', 'technologies', 'sport', 'karpov']}
             onNavClick={onNavClick}
-            currentPage={page}
+            currentCategory={category}
           />
           <div className="footer__bottom">
             <p className="footer__text">

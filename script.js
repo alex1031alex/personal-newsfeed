4af18ae0847cbe2c544f0c7b299b056bad674145
Artur Stambultsian <bumbledos@gmail.com>
2021-09-27 12:26:31 +0300

    correction after screencast

--- script.js
+++ script.js
@@ -8,23 +8,30 @@ const categoryIds = {
 
 const categoryNames = {
   index: 'Главная',
-  sport: 'Спорт',
-  technologies: 'Технологии',
-  karpov: 'Karpov',
   fashion: 'Мода',
+  technologies: 'Технологии',
+  sport: 'Спорт',
+  karpov: 'Karpov'
 }
 
-const Navigation = ({ categories = [], currentCategory, className = '', onNavClick = () => {}, placement, ...restProps }) => {
+const Navigation = ({ onNavClick, currentCategory, className = '' }) => {
   return (
-    <nav {...restProps} className={`grid navigation navigation--${placement} ${className}`}>
-      <a className="navigation__logo" onClick={onNavClick} data-href="index" href="/index.html">
-        <img className="navigation__logo-image" src="./images/logo.svg" alt="Логотип"/>
+    <nav className={`grid navigation ${className}`}>
+      <a className="navigation__logo" data-href="index" href="#">
+        <img className="navigation__logo-image" src="./images/logo.svg" alt="Логотип" />
       </a>
       <ul className="navigation__list">
-        {categories.map((page) => {
+        {['index', 'fashion', 'technologies', 'sport', 'karpov'].map((item) => {
           return (
-            <li className="navigation__item" key={page}>
-              <a className={`navigation__link ${page === currentCategory ? 'navigation__link--active' : ''}`} onClick={onNavClick} data-href={page} href="#">{categoryNames[page]}</a>
+            <li className="navigation__item" key={item}>
+              <a
+                onClick={onNavClick}
+                className={`navigation__link ${currentCategory === item ? 'navigation__link--active' : '' }`}
+                data-href={item}
+                href="#"
+              >
+                {categoryNames[item]}
+              </a>
             </li>
           )
         })}
@@ -33,122 +40,112 @@ const Navigation = ({ categories = [], currentCategory, className = '', onNavCli
   )
 }
 
-const MainArticle = ({ title, image, description, category, source }) => {
+const MainArticle = ({ title, image, category, description, source }) => {
   return (
     <article className="main-article">
       <div className="main-article__image-container">
-        <img className="article-img main-article__img" src={image} alt="Фото новости"/>
+        <img className="article-img main-article__img" src={image} alt="Фото новости" />
       </div>
       <div className="main-article__content">
-        <span className="article-category">{category}</span>
+        <span className="article-category">
+          {category}
+        </span>
         <h2 className="main-article__title">{title}</h2>
         <p className="main-article__text">{description}</p>
-        <span
-          className="article-source main-article__caption">{source}</span>
+        <span className="article-source main-article__caption">
+          {source}
+        </span>
       </div>
     </article>
   )
 }
 
-const SmallArticle = ({ title, date, source }) => {
+const SmallArticle = ({ title, source, date }) => {
   return (
     <article className="small-article">
       <h2 className="small-article__title">{title}</h2>
-      <span
-        className="article-date">{source}</span>
-      <span className="article-source">{new Date(date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' })}</span>
+      <span className="article-date">
+        {source}
+      </span>
+      <span className="article-source">
+        {new Date(date).toLocaleDateString('ru-RU', {
+          month: 'long',
+          day: 'numeric'
+        })}
+      </span>
     </article>
-  )
-}
-
-const Articles = ({ data }) => {
-  return (
-    <section className="articles">
-      <div className="container grid">
-        <section className="articles__big-column">
-          {data.items.slice(0, 3).map(({ title, image, description, category_id, source_id }) => {
-            return (
-              <MainArticle
-                key={title}
-                title={title}
-                image={image}
-                description={description}
-                category={data.categories.find(({id}) => category_id === id).name}
-                source={data.sources.find(({id}) => source_id === id).name}
-              />
-            )
-          })}
-        </section>
-        <section className="articles__small-column">
-          {data.items.slice(3, 12).map(({ title, date, source_id  }) => {
-            return (
-              <SmallArticle
-                key={title}
-                title={title}
-                date={date}
-                source={data.sources.find(({id}) => source_id === id).name}
-              />
-            )
-          })}
-        </section>
-      </div>
-    </section>
   )
 }
 
 const App = () => {
   const [category, setCategory] = React.useState('index');
-  const [data, setData] = React.useState({ items: [], categories: [], sources: [] });
-
-  React.useEffect(() => {
-    fetch(`https://frontend.karpovcourses.net/api/v2/ru/news/${categoryIds[category]}`)
-      .then(response => response.json())
-      .then(({ items, categories, sources }) => {
-        setData({ items, categories, sources })
-      })
-  }, [category]);
+  const [articles, setArticles] = React.useState({ items: [], categories: [], sources: [] });
 
   const onNavClick = (e) => {
     e.preventDefault();
     setCategory(e.currentTarget.dataset.href);
   }
 
+  React.useEffect(() => {
+    fetch('https://frontend.karpovcourses.net/api/v2/ru/news/' + categoryIds[category] || '')
+      .then(response => response.json())
+      .then((response) => {
+        setArticles(response);
+      })
+  }, [category])
+
   return (
     <React.Fragment>
       <header className="header">
         <div className="container">
-          <Navigation
-            placement="header"
-            categories={['index', 'fashion', 'technologies', 'sport', 'karpov']}
-            onNavClick={onNavClick}
-            currentCategory={category}
-          />
+          <Navigation onNavClick={onNavClick} currentCategory={category} className="header__navigation" />
         </div>
       </header>
 
       <main>
-        <Articles data={data}/>
+        <section className="articles">
+          <div className="container grid">
+            <section className="articles__big-column">
+              {articles.items.slice(0, 3).map((item) => {
+                return (
+                  <MainArticle
+                    key={item.title}
+                    title={item.title}
+                    description={item.description}
+                    image={item.image}
+                    category={articles.categories.find(({id}) => item.category_id === id).name}
+                    source={articles.sources.find(({id}) => item.source_id === id).name}
+                  />
+                )
+              })}
+            </section>
+            <section className="articles__small-column">
+              {articles.items.slice(3, 12).map((item) => {
+                return (
+                  <SmallArticle
+                    key={item.title}
+                    title={item.title}
+                    source={articles.sources.find(({id}) => item.source_id === id).name}
+                    date={item.date}
+                  />
+                )
+              })}
+            </section>
+          </div>
+        </section>
       </main>
 
       <footer className="footer">
         <div className="container">
-          <Navigation
-            placement="footer"
-            className="footer__navigation"
-            categories={['index', 'fashion', 'technologies', 'sport', 'karpov']}
-            onNavClick={onNavClick}
-            currentCategory={category}
-          />
+          <Navigation onNavClick={onNavClick} currentCategory={category} className="footer__navigation" />
           <div className="footer__bottom">
-            <p className="footer__text">
-              Сделано на Frontend курсе в <a className="footer__link" href="https://karpov.courses/frontend" target="_blank">Karpov.Courses</a>
-            </p>
+            <p className="footer__text">Сделано на Frontend курсе в <a className="footer__link" href="https://karpov.courses/frontend" target="_blank">Karpov.Courses</a></p>
             <p className="footer__text footer__text--gray">© 2021</p>
           </div>
         </div>
       </footer>
     </React.Fragment>
-  );
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));

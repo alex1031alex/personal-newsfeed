385646e083932f5b0e7b10853058aedc8a8419b3
d.bezuglyj <d.e.bezugly@gmail.com>
2021-09-28 17:31:11 +0300

    Создал статичный компонент статьи

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './Components/App/App.js';
 import './common.css';
+import { App } from './Components/App/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));

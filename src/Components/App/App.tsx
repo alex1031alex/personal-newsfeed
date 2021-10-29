c0526b06eae76d4290e47301eeed37621ceb9eba
d.bezuglyj <d.e.bezugly@gmail.com>
2021-10-29 14:26:52 +0300

    Заверстали админку партнерских новостей

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,12 @@
-import React, { FC, Fragment, useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
-import './App.css';
 
-import { Navigation } from '../Navigation/Navigation';
 import { Articles } from '../Articles/Articles';
 import { ArticleItem } from '../ArticleItem/ArticleItem';
+import { Page } from '../Page/Page';
+import { AdminPage } from '../AdminPage/AdminPage';
+import { AdminArticles } from '../AdminArticles/AdminArticles';
+import { AdminArticleItem } from '../AdminArticleItem/AdminArticleItem';
 
 export const App: FC = () => {
   const { pathname } = useLocation();
@@ -14,41 +16,37 @@ export const App: FC = () => {
   }, [pathname]);
 
   return (
-    <Fragment>
-      <header className="header">
-        <div className="container">
-          <Navigation placement="header" className="header__navigation" />
-        </div>
-      </header>
-
-      <main>
-        <Switch>
-          <Route path="/article/:id">
-            <ArticleItem />
-          </Route>
-          <Route path="/:categoryId">
-            <Articles />
-          </Route>
-          <Route path="/">
-            <Articles />
-          </Route>
-        </Switch>
-      </main>
-
-      <footer className="footer">
-        <div className="container">
-          <Navigation placement="footer" className="footer__navigation" />
-          <div className="footer__bottom">
-            <p className="footer__text">
-              Сделано на Frontend курсе в{' '}
-              <a className="footer__link" href="https://karpov.courses/frontend" target="_blank" rel="noreferrer">
-                Karpov.Courses
-              </a>
-            </p>
-            <p className="footer__text footer__text--gray">© 2021</p>
-          </div>
-        </div>
-      </footer>
-    </Fragment>
+    <Switch>
+      <Route path="/admin" exact>
+        <AdminPage>
+          <AdminArticles />
+        </AdminPage>
+      </Route>
+      <Route path="/admin/create">
+        <AdminPage>
+          <AdminArticleItem />
+        </AdminPage>
+      </Route>
+      <Route path="/admin/edit/:id">
+        <AdminPage>
+          <AdminArticleItem />
+        </AdminPage>
+      </Route>
+      <Route path="/article/:id">
+        <Page>
+          <ArticleItem />
+        </Page>
+      </Route>
+      <Route path="/:categoryId">
+        <Page>
+          <Articles />
+        </Page>
+      </Route>
+      <Route path="/">
+        <Page>
+          <Articles />
+        </Page>
+      </Route>
+    </Switch>
   );
 };

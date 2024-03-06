c54a0d336d399daba029215bc0177ae1a4ffa33b
Alex <alextitkevich10@yahoo.com>
2024-03-06 20:26:08 +0300

    Обновил стили шапки на мобильной версии

---
+++ src/components/Header/MobileHeader.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from "react";
+import classNames from "classnames";
+import { Logo } from "@components/Logo/Logo";
+import { Navigation } from "@components/Navigation/Navigation";
+import { CSSTransition } from "react-transition-group";
+import { Burger } from "@components/Icons/Burger";
+import { Cross } from "@components/Icons/Cross";
+import { ColorSchemeSwitcherMobile } from "@features/colorScheme/components/ColorSchemeSwitcherMobile/ColorSchemeSwitcherMobile";
+
+export const MobileHeader: FC = () => {
+  const [isOpenMenu, toggleMenu] = useState(false);
+  const [isOpenSubMenu, toggleSubMenu] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="container header__mobile-container">
+        <Logo />
+        <button className="header__mobile-button" onClick={() => toggleMenu(!isOpenMenu)}>
+          {isOpenMenu ? <Cross /> : <Burger />}
+        </button>
+      </div>
+      <CSSTransition in={isOpenMenu} mountOnEnter unmountOnExit timeout={200} classNames="header-mobile-menu-animation">
+        <div className="header__mobile-menu">
+          {isOpenSubMenu ? (
+            <button className="header__mobile-back-button" onClick={() => toggleSubMenu(false)}>
+              К меню
+            </button>
+          ) : (
+            <Navigation className="header__mobile-navigation" />
+          )}
+
+          <div className={classNames("header__mobile-controls", { "header__mobile-controls--hasMenu": isOpenSubMenu })}>
+            <ColorSchemeSwitcherMobile isMenuActive={isOpenSubMenu} onClickSchemeButton={() => toggleSubMenu(true)} />
+          </div>
+        </div>
+      </CSSTransition>
+    </header>
+  );
+};
--- src/components/Icons/Cross.tsx
+++ src/components/Icons/Cross.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 export const Cross: FC = () => {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5 5L19 19M19 5L5 19" stroke="black" strokeWidth="2.5" strokeLinecap="round" />
+      <path d="M5 5L19 19M19 5L5 19" stroke="var(--foreground-primary)" strokeWidth="2.5" strokeLinecap="round" />
     </svg>
   );
 };
